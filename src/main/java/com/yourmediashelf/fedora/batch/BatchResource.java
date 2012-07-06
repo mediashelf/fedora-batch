b5c2c2ab64eda7191d1c94b8ea50401197ee65b3
Edwin Shin <edwin.shin@yourmediashelf.com>
2012-07-06 15:49:23 +0800

    First pass at a batch purge datastreams method

--- src/main/java/com/yourmediashelf/fedora/batch/BatchResource.java
+++ src/main/java/com/yourmediashelf/fedora/batch/BatchResource.java
@@ -21,24 +21,30 @@ package com.yourmediashelf.fedora.batch;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 
 import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import org.apache.cxf.jaxrs.ext.multipart.MultipartBody;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.fcrepo.common.Constants;
 import org.fcrepo.server.Context;
 import org.fcrepo.server.Server;
 import org.fcrepo.server.errors.ServerException;
 import org.fcrepo.server.rest.BaseRestResource;
 import org.fcrepo.server.storage.types.MIMETypedStream;
+import org.fcrepo.utilities.DateUtility;
 import org.springframework.stereotype.Component;
 
 /**
@@ -55,7 +61,7 @@ public class BatchResource extends BaseRestResource implements Constants {
     }
     
     /**
-     * <p>Get the datastream content denoted by dsids from the objects denoted.
+     * <p>Get the specified datastream content from the selected objects.
      * 
      * @param pidList a List of pids (pidList will be union'ed with pidsCSV)
      * @param dsidList a List of datastream ids (dsidList will be union'ed with dsidsCSV)
@@ -72,24 +78,38 @@ public class BatchResource extends BaseRestResource implements Constants {
             @QueryParam("pids") String pidsCsv, 
             @QueryParam("dsids") String dsidsCsv) {
         
-        Set<String> pids = new HashSet<String>();
-        Set<String> dsids = new HashSet<String>();
-        if (pidList != null) {
-            pids.addAll(pidList);
-        }
-        if (pidsCsv != null && !pidsCsv.trim().isEmpty()) {
-            pids.addAll(Arrays.asList(pidsCsv.split(",")));
-        }
-        if (dsidList != null) {
-            dsids.addAll(dsidList);
-        }
-        if (dsidsCsv != null && !dsidsCsv.trim().isEmpty()) {
-            dsids.addAll(Arrays.asList(dsidsCsv.split(",")));
-        }
+        Set<String> pids = combine(pidList, pidsCsv);
+        Set<String> dsids = combine(dsidList, dsidsCsv);
         return getDatastreams(pids, dsids);
     }
     
-    public MultipartBody getDatastreams(Set<String> pids, Set<String> dsids) {
+    /**
+     * <p>Delete the specified datastreams from the selected objects.
+     * 
+     * @param pidList a List of pids (pidList will be union'ed with pidsCSV)
+     * @param dsidList a List of datastream ids (dsidList will be union'ed with dsidsCSV)
+     * @param pidsCsv a comma-separated list of pids
+     * @param dsidsCsv a comma-separated list of datastream ids
+     * @return a multipart/mixed response
+     */
+    @Path("/deleteDatastreams")
+    @Produces("multipart/mixed")
+    @DELETE
+    public MultipartBody deleteDatastreams(
+            @QueryParam("pid") List<String> pidList, 
+            @QueryParam("dsid") List<String> dsidList,
+            @QueryParam("pids") String pidsCsv, 
+            @QueryParam("dsids") String dsidsCsv,
+            @QueryParam("startDT") String startDT,
+            @QueryParam("endDT") String endDT,
+            @QueryParam("logMessage") String logMessage) {
+        
+        Set<String> pids = combine(pidList, pidsCsv);
+        Set<String> dsids = combine(dsidList, dsidsCsv);
+        return deleteDatastreams(pids, dsids, startDT, endDT, logMessage);
+    }
+    
+    private MultipartBody getDatastreams(Set<String> pids, Set<String> dsids) {
         List<Attachment> atts =
                 new ArrayList<Attachment>(pids.size() * dsids.size());
 
@@ -110,5 +130,50 @@ public class BatchResource extends BaseRestResource implements Constants {
             }
         }
         return new MultipartBody(atts, true);
+    }
+    
+    private MultipartBody deleteDatastreams(Set<String> pids, Set<String> dsids, 
+            String startDT, String endDT, String logMessage) {
+        List<Attachment> atts =
+                new ArrayList<Attachment>(pids.size() * dsids.size());
+
+        Context context = getContext();
+        Date[] purged = null;
+        ObjectMapper mapper = new ObjectMapper();
+
+        for (String pid : pids) {
+            for (String dsid : dsids) {
+                try {
+                    purged =
+                            m_management.purgeDatastream(context, pid, dsid,
+                                    DateUtility.parseDateOrNull(startDT),
+                                    DateUtility.parseDateOrNull(endDT),
+                                    logMessage);
+
+                    // convert purged into a String[] so we can return it as a JSON array
+                    List<String> results = new ArrayList<String>();
+                    for (Date d : purged) {
+                        results.add(DateUtility.convertDateToXSDString(d));
+                    }
+                    atts.add(new Attachment(pid + "/" + dsid,
+                            MediaType.APPLICATION_JSON, mapper
+                                    .writeValueAsString(results)));
+                } catch (Exception e) {
+                    throw new WebApplicationException(handleException(e, false));
+                }
+            }
+        }
+        return new MultipartBody(atts, true);
+    }
+    
+    private Set<String> combine(Collection<String> coll, String csv) {
+        Set<String> combined = new HashSet<String>();
+        if (coll != null) {
+            combined.addAll(coll);
+        }
+        if (csv != null && !csv.trim().isEmpty()) {
+            combined.addAll(Arrays.asList(csv.split(",")));
+        }
+        return combined;
     }
 }
